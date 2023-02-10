39bac85a3e8406511fd710d0b4307d3c780af0ec
kauser_bano <bkauser777@gmail.com>
2023-02-10 11:04:35 -0800

    first commit

---
+++ bankk.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.6;
+
+contract Bank {
+
+    mapping(address => uint)balance;
+
+
+
+    modifier onlyOwner{
+
+          require(owner ==msg.sender,"you are not the owner")'
+          _;
+
+    }
+    function addBalance(uint _toAdd)public returns (uint) onlyOwner{
+        balance[msg.sender] += _toAdd;
+        return balance[msg.sender];
+
+    }
+
+    function getBalance()public view returns(uint){
+        return balance[msg.sender];
+
+    }
+    function transfer(address recipient, uint amount)public onlyOwner{
+       
+        require(balance[msg.sender] >= amount,"you dont have enough balance to send")
+           require(msg.sender!= recipient )
+        balance[msg.sender] -= amount;
+        balance[recipient] += amount;
+
+    }
+}
